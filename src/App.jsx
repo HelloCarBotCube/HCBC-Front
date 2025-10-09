470ddaeb83aa2cc5b0fab09c9957850f9614f073
koreahghg <koreahghg@gmail.com>
2025-10-09 16:18:08 +0900

    Profile

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,10 @@
 import { Routes, Route } from "react-router-dom";
-import Login from "./login/login";
-import Signup from "./SignUp/Signup";
-import Main from "./main/Main";
 import Profile from "./profile/Profile";
-import "./SignUp/index.css";
-import "./login/index.css";
-import "./main/index.css";
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Login />} />
-      <Route path="/signup" element={<Signup />} />
-      <Route path="/main" element={<Main />} />
-      <Route path="/profile" element={<Profile />} />
+      <Route path="/" element={<Profile />} />
     </Routes>
   );
 }
--- src/main/Main.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./index.css";
-
-function Main() {
-  return <div className="main-page">로그인 성공</div>;
-}
-
-export default Main;
