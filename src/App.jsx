012a5264ea681171ecc8c03a6e29347ad080ac16
koreahghg <koreahghg@gmail.com>
2025-11-02 14:54:00 +0900

    메인 경로 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import { Routes, Route } from "react-router-dom";
 import Login from "./login/Login";
 import Signup from "./SignUp/Signup";
+import Main from "./main/Main";
+import Profile from "./profile/Profile";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Login />} />
       <Route path="/signup" element={<Signup />} />
+      <Route path="/main" element={<Main />} />
+      <Route path="/profile" element={<Profile />} />
     </Routes>
   );
 }
