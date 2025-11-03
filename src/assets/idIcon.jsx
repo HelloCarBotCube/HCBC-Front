00d2995dcc8f51160471ea6eb7d449780294fa78
MOON KANGHYEON <s25024@gsm.hs.kr>
2025-11-03 18:13:18 +0900

    Revert "Feat/signup"

--- src/assets/idIcon.jsx
+++ src/assets/idIcon.jsx
@@ -11,12 +11,12 @@ export default function IdIcon() {
       <path
         d="M12 10C14.2091 10 16 8.20914 16 6C16 3.79086 14.2091 2 12 2C9.79086 2 8 3.79086 8 6C8 8.20914 9.79086 10 12 10Z"
         stroke="#D6D6D7"
-        strokeWidth="1.5"
+        stroke-width="1.5"
       />
       <path
         d="M20 17.5C20 19.985 20 22 12 22C4 22 4 19.985 4 17.5C4 15.015 7.582 13 12 13C16.418 13 20 15.015 20 17.5Z"
         stroke="#D6D6D7"
-        strokeWidth="1.5"
+        stroke-width="1.5"
       />
     </svg>
   );
