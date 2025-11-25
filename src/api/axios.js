28dcd4c1fbd5fe54948881c011c50407db95a5b0
moonkanghyeon <s25024@gsm.hs.kr>
2025-11-25 18:29:59 +0900

    feat: 토큰 자동 재발급 기능 추가
    - axios interceptor에 401 에러 처리 로직 추가
    - refreshToken을 이용한 자동 토큰 갱신 구현
    - 동시 요청 처리를 위한 큐 관리 시스템 추가
    - 재발급 실패 시 로그인 페이지로 리다이렉트

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,13 +1,33 @@
-import axios from "axios";
-import { getAccessToken } from "../utils/cookies";
+import axios from 'axios';
+import {
+  getAccessToken,
+  getRefreshToken,
+  setAccessToken,
+  setRefreshToken,
+  clearTokens,
+} from '../utils/cookies';
 
 const instance = axios.create({
-  baseURL: "http://gsmsv-1.yujun.kr:27919",
+  baseURL: 'http://gsmsv-1.yujun.kr:27919',
   timeout: 10000,
   headers: {
-    "Content-Type": "application/json",
+    'Content-Type': 'application/json',
   },
 });
+
+let isRefreshing = false;
+let failedQueue = [];
+
+const processQueue = (error, token = null) => {
+  failedQueue.forEach((prom) => {
+    if (error) {
+      prom.reject(error);
+    } else {
+      prom.resolve(token);
+    }
+  });
+  failedQueue = [];
+};
 
 instance.interceptors.request.use(
   (config) => {
@@ -26,11 +46,73 @@ instance.interceptors.response.use(
   (response) => {
     return response;
   },
-  (error) => {
+  async (error) => {
+    const originalRequest = error.config;
+
+    if (error.response?.status === 401 && !originalRequest._retry) {
+      if (isRefreshing) {
+        return new Promise((resolve, reject) => {
+          failedQueue.push({ resolve, reject });
+        })
+          .then((token) => {
+            originalRequest.headers.Authorization = `Bearer ${token}`;
+            return instance(originalRequest);
+          })
+          .catch((err) => {
+            return Promise.reject(err);
+          });
+      }
+
+      originalRequest._retry = true;
+      isRefreshing = true;
+
+      const refreshToken = getRefreshToken();
+
+      if (!refreshToken) {
+        clearTokens();
+        window.location.href = '/';
+        return Promise.reject(error);
+      }
+
+      try {
+        const response = await axios.post(
+          'http://gsmsv-1.yujun.kr:27919/api/auth/reissue',
+          {},
+          {
+            headers: {
+              refreshToken: refreshToken,
+            },
+          }
+        );
+
+        const {
+          accessToken,
+          refreshToken: newRefreshToken,
+          accessTokenExpiresIn,
+          refreshTokenExpiresIn,
+        } = response.data;
+
+        setAccessToken(accessToken);
+        setRefreshToken(newRefreshToken);
+
+        instance.defaults.headers.common['Authorization'] = `Bearer ${accessToken}`;
+        originalRequest.headers.Authorization = `Bearer ${accessToken}`;
+
+        processQueue(null, accessToken);
+
+        return instance(originalRequest);
+      } catch (refreshError) {
+        processQueue(refreshError, null);
+        clearTokens();
+        window.location.href = '/';
+        return Promise.reject(refreshError);
+      } finally {
+        isRefreshing = false;
+      }
+    }
+
     if (error.response) {
       switch (error.response.status) {
-        case 401:
-          break;
         case 403:
           break;
         case 404:
